f77d608d78526a5678803ec3f8cb101e8c4b0914
braincee <kwesi938@gmail.com>
2022-07-22 19:33:26 +0100

    rubocop fix errors

--- classroom.rb
+++ classroom.rb
@@ -1,14 +1,13 @@
-class classroom
+class Classroom
+  attr_accessor :label
+  attr_reader :students
 
-    attr_accessor :label
-    attr_reader :students
+  def initialize(label)
+    @label = label
+    @students = []
+  end
 
-    def initialize(label)
-        @label = label
-        @students =[]
-    end
-
-    def add_student(student)
-        @student << student
-    end
+  def add_student(student)
+    @student << student
+  end
 end
