80c127867248cf521f43355d5d18c2ce49e2bc62
braincee <kwesi938@gmail.com>
2022-07-22 12:41:25 +0100

    Inherits person class and extends constructor by adding specialization and a parameter to it

--- teacher.rb
+++ teacher.rb
@@ -1,2 +1,9 @@
+require './person.rb'
+
 class Teacher
+    def initialize(age, name = 'Unknown', parent_permission: true, specialization)
+        super(age, name, parent_permission)
+        @specialization = specialization
+    end
+    
 end
