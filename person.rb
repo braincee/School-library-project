068d6e116bace21fa43b929874f9f4645a8b20c8
braincee <kwesi938@gmail.com>
2022-07-22 16:31:29 +0100

    fix linters

--- person.rb
+++ person.rb
@@ -2,6 +2,7 @@ require './nameable'
 
 class Person < Nameable
   def initialize(age, name = 'Unknown', parent_permission: true)
+    super()
     @id = Random.rand(1..100)
     @name = name
     @age = age
