21e73b1901ba09af68ebb077b0415b3d22ac56a5
braincee <kwesi938@gmail.com>
2022-07-22 12:34:03 +0100

    Inherits from person class and extends constructor by add classroom and a paramenter to it

--- student.rb
+++ student.rb
@@ -1,2 +1,9 @@
-class Student
+require './person.rb'
+
+class Student < Person
+ 
+    def initialize(age, name, parent_permission: true, classroom)
+        super(age, name, parent_permission)
+        @classroom = classroom
+    end
 end 
