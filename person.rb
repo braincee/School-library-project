3fdcb0c2cbc47247b61cce442607e2d2e7452985
braincee <kwesi938@gmail.com>
2022-07-22 12:18:28 +0100

    create instance variables and constructors

--- person.rb
+++ person.rb
@@ -1,4 +1,9 @@
 class Person
 
-
+def initialize(age, name='Unknown', parent_permission: true)
+        @id = Random.rand(1..100)
+        @name = name 
+        @age = age
+        @parent_permission = parent_permission
+    end
 end
