5f4b1ce2668bd3c28bc9e394f2f5f65f76a44aac
braincee <kwesi938@gmail.com>
2022-07-23 19:30:18 +0100

    Use rubocop to fix linters

--- main.rb
+++ main.rb
@@ -1,48 +1,7 @@
-class App
-  def initialize
-    @people = []
-    @books = []
-  end
-
-   def choices
-    puts '1  List all book'
-    puts '2  List all People'
-    puts '3  Create a person'
-    puts '4  Create a book'
-    puts '5  Create a rental'
-    puts '6  List all rentals for a given person id'
-    puts '7  Exit'
-  end
-
-   def make(choices)
-  end
-
-    def all_books
-  end
-
-  def all_people
-  end
-
-   def create_person
-  end
-
-   def create_teacher
-  end
-
-  def create_student
-  end
-
-   def create_book
-  end
-
-  def create_rental
-  end
-
-   def list_of_rental
-    
-end
-
-def main
-end
-
-end
+require './teacher'
+require './student'
+require './person'
+require './book'
+require './capitalize_decorator'
+require './trimmer_decorator'
+require './rental'
