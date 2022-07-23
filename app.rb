b67826408d7e29fb305e39b3e3c4410b7612544e
braincee <kwesi938@gmail.com>
2022-07-23 13:22:17 +0100

    Create method to list all books

---
+++ app.rb
@@ -0,0 +1,20 @@
+require './book'
+require './classroom'
+require './base_decorator'
+require './person'
+require './rental'
+require './student'
+require './teacher'
+
+class App
+  def initialize
+    @books = []
+    @people = []
+    @rentals = []
+  end
+
+  def books_list
+    
+  end
+end
+
