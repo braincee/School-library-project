87886e15cd686cf0df27470a4ecafeb79eef9d0b
braincee <kwesi938@gmail.com>
2022-07-25 10:21:21 +0100

    changes to ui

--- person.rb
+++ person.rb
@@ -1,36 +1,29 @@
-# rubocop:disable Naming/PredicateName
 require './nameable'
-require './rental'
 
 class Person < Nameable
   attr_reader :id, :rentals
-  attr_accessor :name, :age, :parent_permission
+  attr_accessor :name, :age
 
-  def initialize(age, name = 'Unknown', parent_permission: true)
-    @id = Random.rand(1...1000)
-    @name = name
+  def initialize(age, name = 'Unknown', parent_permission = 'true')
+    super()
+    @id = Random.rand(1..1000)
     @age = age
+    @name = name
     @parent_permission = parent_permission
     @rentals = []
-    super()
   end
 
   def can_use_services?
-    @parent_permission || is_of_age?
+    of_age? || @parent_permission
   end
 
   def correct_name
     @name
   end
 
-  def add_rental(date, book)
-    Rental.new(date, self, book)
-  end
-
   private
 
-  def is_of_age?
-    @age > 18
+  def of_age?
+    @age >= 18
   end
 end
-# rubocop:enable Naming/PredicateName
