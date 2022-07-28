5263efeffafd100667e166d76e19335c6071c1ac
braincee <kwesi938@gmail.com>
2022-07-28 13:13:09 +0100

    Add student file for file

---
+++ spec/student_spec.rb
@@ -0,0 +1,16 @@
+require_relative('../classroom')
+require_relative('../student')
+
+describe Student do
+  before :each do
+    @classroom = Classroom.new ('Physics Classroom')
+    @student = Student.new('777', 23, 'James Clear', parent_permission: true)
+  end
+  it 'should return specified string' do
+    expect(@student.play_hooky).to eq  "¯\(ツ)/¯"
+  end
+  it 'should return specified string' do
+    @classroom.add_student(@student)
+    expect(@classroom.students.length).to be(1)
+  end
+end
