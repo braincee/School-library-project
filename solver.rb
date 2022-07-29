677abb09dc706172907ca2600702081fef4d376b
ericmahare <erikmahare@gmail.com>
2022-07-29 13:54:03 +0300

    Add factorial functionality following the TDD testing

--- solver.rb
+++ solver.rb
@@ -1,4 +1,9 @@
 class Solver
-  def factorial
+  def factorial(n)
+    return 'Error' if n == nil?
+    return 'Wrong value specified' if n < 0
+    return n * factorial(n - 1)  if n > 0 
+    return 1 if n == 0
   end
 end
+
