3f925536d7c547b64d84a663dcb583fb2993897f
ericmahare <erikmahare@gmail.com>
2022-07-29 14:40:00 +0300

    Fix linter errors

--- solver.rb
+++ solver.rb
@@ -1,21 +1,23 @@
 class Solver
-  def factorial(n)
-    return 'Error' if n == nil?
-    return 'Wrong value specified' if n < 0
-    return n * factorial(n - 1)  if n > 0 
-    return 1 if n == 0
+  def factorial(number)
+    return 'Error' if number == nil?
+    return 'Wrong value specified' if number.negative?
+    return number * factorial(number - 1) if number.positive?
+    return 1 if number.zero?
   end
 
   def reverse(string)
-    return "Not a string" if string.class != String
-    return "No value specified" if string.empty? == true
+    return 'Not a string' if string.class != String
+    return 'No value specified' if string.empty? == true
+
     string.reverse
   end
 
-  def fizzbuzz(n)
-    return "fizzbuzz" if n % 3 == 0 && n % 5 == 0
-    return "fizz" if (n % 3).zero?
-    return "buzz" if n % 5 == 0
-    "#{n}"
-  end 
+  def fizzbuzz(number)
+    return 'fizzbuzz' if (number % 3).zero? && (number % 5).zero?
+    return 'fizz' if (number % 3).zero?
+    return 'buzz' if (number % 5).zero?
+
+    number.to_s
+  end
 end
